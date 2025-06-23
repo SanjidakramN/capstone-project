4a9907d9f68cc6c23fc6abd9e0938e5f75ea90d2
SanjidakramN <sanjidakram2@gmail.com>
2025-06-23 19:39:33 +0530

    Update backend.tf

--- terraform_new/backend.tf
+++ terraform_new/backend.tf
@@ -1,10 +1,9 @@
-
 terraform {
   backend "s3" {
-    bucket         = "sanju-terraform-state-eks"         # ✅ Replace with your actual S3 bucket name
+    bucket         = "sanju-terraform-state-eks"
     key            = "eks-rds-fullstack/terraform.tfstate"
     region         = "us-east-1"
-    dynamodb_table = "terraform-lock-table"              # ✅ Make sure this exists
+    dynamodb_table = "terraform-lock-table"
     encrypt        = true
   }
 }
